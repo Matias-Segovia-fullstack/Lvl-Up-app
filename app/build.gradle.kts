6ee6dd102510c4b0e506efbd3c846000e716b7ce
zalo <gonz.pino@duocuc.cl>
2025-10-19 18:33:02 -0300

    Correcion de dependencias compatibilidad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,10 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.androidx.room.ktx) // Usa la versión 2.8.2 del catálogo
+    kapt("androidx.room:room-compiler:2.8.2")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,8 +61,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.room:room-ktx:2.6.1")
-    kapt("androidx.room:room-compiler:2.6.1")
-
 }
+
+
+
