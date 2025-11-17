7b99e0398e92d60e7a96a52ba79de100d7e3f99d
Matias <mati.segovia@duocuc.cl>
2025-11-17 01:53:50 -0300

    arreglo de viewmodelfactory

--- app/src/main/java/com/example/lvl_up/viewmodel/CarritoViewModelFactory.kt
+++ app/src/main/java/com/example/lvl_up/viewmodel/CarritoViewModelFactory.kt
@@ -5,15 +5,12 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.lvl_up.data.CarritoRepository
 import com.example.lvl_up.data.ProductRepository
 
-
 class CarritoViewModelFactory(
     private val carritoRepository: CarritoRepository,
-    private val productRepository: ProductRepository // ✅ AÑADE EL REPOSITORIO DE PRODUCTOS
-) : ViewModelProvider.Factory {
+    private val productRepository: ProductRepository) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(CarritoViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            // ✅ PÁSALE EL NUEVO REPOSITORIO AL VIEWMODEL
             return CarritoViewModel(carritoRepository, productRepository) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
